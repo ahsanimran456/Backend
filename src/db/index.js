6899cb01c5a099dfd786029705db786f2255ac9e
ahsan imran <ahxanimran456@gmail.com>
2024-04-12 16:54:57 +0500

    connected db

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import { DB_Name } from '../constant.js';
+
+
+const ConnectDB = async () => {
+    try {
+        const DBConnection = await mongoose.connect(`${process.env.DB_URL}/${DB_Name}`);
+        console.log(DBConnection.connection.host, "<------db connected---->");
+    } catch (error) {
+        console.log(`Mongodb error : ${error} `);
+    }
+}
+
+
+export default ConnectDB
--- src/index.js
+++ src/index.js
@@ -1,39 +1,17 @@
 import express from 'express'
 import { config } from 'dotenv';
+import ConnectDB from './db/index.js';
 config();
 const app = express()
 const env = process.env
 
+ConnectDB()
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.get('/get', (req, res) => {
-    const data = [
-        "smartphones",
-        "laptops",
-        "fragrances",
-        "skincare",
-        "groceries",
-        "home-decoration",
-        "furniture",
-        "tops",
-        "womens-dresses",
-        "womens-shoes",
-        "mens-shirts",
-        "mens-shoes",
-        "mens-watches",
-        "womens-watches",
-        "womens-bags",
-        "womens-jewellery",
-        "sunglasses",
-        "automotive",
-        "motorcycle",
-        "lighting"
-    ]
-    res.send(data)
-})
+
 
 app.listen(env.PORT, () => {
     console.log(`Example app listening on port ${env.PORT}`)
