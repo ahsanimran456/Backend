e29f4d235ef1fc24fe71019ff800d287be8adf2f
ahsan imran <ahxanimran456@gmail.com>
2024-04-13 13:50:13 +0500

    fixe app

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import express from 'express'
 import { config } from 'dotenv';
 import ConnectDB from './db/index.js';
+import app from './app.js';
 config();
-const app = express()
 const env = process.env
 
 ConnectDB()
+    .then(() => {
+        app.listen(env.PORT, () => {
+            console.log(`App listening on port ${env.PORT}`)
+        })
+    }).catch((err) => {
+        console.log(`Mongodb Connection Failed error: ${err}`);
+    })
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-})
-
-
-
-app.listen(env.PORT, () => {
-    console.log(`Example app listening on port ${env.PORT}`)
 })
